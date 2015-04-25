c0ad123cb1d398ca7cd4ea986c0dbd2539616bc4
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-04-25 15:47:41 +0300

    Made examples work when run from a different dir

--- examples/hp/tryme.sh
+++ examples/hp/tryme.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sqawk="$(dirname "$(readlink -f "$0")")/../../sqawk.tcl"
+dir="$(dirname "$(readlink -f "$0")")"
+sqawk="$dir/../../sqawk.tcl"
+pushd $dir > /dev/null
 "$sqawk" 'select a1, b1, a2 from a inner join b on a2 = b2 where b1 < 10000 order by b1' MD5SUMS du-bytes
+popd > /dev/null
--- examples/three-files/tryme.sh
+++ examples/three-files/tryme.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sqawk="$(dirname "$(readlink -f "$0")")/../../sqawk.tcl"
+dir="$(dirname "$(readlink -f "$0")")"
+sqawk="$dir/../../sqawk.tcl"
+pushd $dir > /dev/null
 "$sqawk" -FS , 'select a1, a2, b2, c2 from a inner join b on a1 = b1 inner join c on a1 = c1' 1 2 3
+popd > /dev/null
