3bd9fba8a089ae08eff4cb923b1b1ec6dbce951c
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-09-01 20:54:36 +0300

    vagrant/bootstrap.sh: Fixed initial provisioning

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
-if ! dpkg -s tcl >/dev/null \
-    && dpkg -s tcllib >/dev/null \
-    && dpkg -s libsqlite3-tcl >/dev/null; then
-  sudo apt-get update
-  sudo apt-get -y install tclsh tcllib libsqlite3-tcl
+is_installed() {
+  dpkg-query -Wf'${db:Status-abbrev}' "$1" 2>/dev/null | grep -q '^i'
+}
+
+# Install the Tcl packages if needed.
+if ! is_installed tcl && is_installed tcllib && is_installed libsqlite3-tcl
+then
+  apt-get update
+  apt-get -y install tclsh tcllib libsqlite3-tcl
 fi
 
-mkdir -p /opt
-rm -rf /opt/sqawk
-cp -r /sqawk /opt/sqawk
-cd /opt/sqawk
+# Install or reinstall Sqawk.
+rm -rf /tmp/sqawk
+cp -r /sqawk /tmp/sqawk
+cd /tmp/sqawk
 make
-sudo make install
+make install
+
+# Check if Sqawk works and reports its version correctly.
+reported_version=$(sqawk -v).1
+source_version=$(awk '/version/{print $3}' /tmp/sqawk/sqawk.tcl | head -n 1)
+
+if test "$reported_version" = "$source_version"
+then
+  echo "Sqawk version: $reported_version"
+else
+  echo "error: reported version doesn't match source code version:"
+  echo "\"$reported_version\" != \"$source_version\""
+  exit 1
+fi
