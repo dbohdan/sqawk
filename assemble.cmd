2c1c324a3573bda5151ae349a71b85429761f6cc
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-05-10 18:13:09 +0300

    assemble.cmd: Allow user to override tclsh choice

--- assemble.cmd
+++ assemble.cmd
@@ -1,4 +1,13 @@
 @echo off
 set tclsh=tclsh
 if exist c:\Tcl\bin\tclsh.exe set tclsh=c:\Tcl\bin\tclsh.exe
+
+rem Allow the user to override which Tcl binary we use.
+set arg=%1%
+if "%arg%"=="" goto do_assemble
+if "%arg:~0,7%"=="/tclsh:" (
+    set tclsh=%arg:~7%
+)
+
+:do_assemble
 %tclsh% tools/assemble.tcl sqawk-dev.tcl > sqawk.tcl
