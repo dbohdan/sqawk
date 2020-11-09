15775b6161c4149a5d515c3d6cd9a8b3a7cbbaa1
D. Bohdan <dbohdan@dbohdan.com>
2020-11-09 06:44:54 +0000

    refactor(parsers/tcl): use early returns

--- lib/parsers/tcl.tcl
+++ lib/parsers/tcl.tcl
@@ -1,5 +1,5 @@
 # Sqawk, an SQL Awk.
-# Copyright (c) 2015, 2016, 2017, 2018 dbohdan
+# Copyright (c) 2015, 2016, 2017, 2018, 2020 D. Bohdan
 # License: MIT
 
 namespace eval ::sqawk::parsers::tcl {
@@ -21,7 +21,7 @@ namespace eval ::sqawk::parsers::tcl {
 
     constructor {channel options} {
         set useDicts [dict get $options dicts]
-        set i [expr {$useDicts ? -1 : 0}]
+        set i [expr { $useDicts ? -1 : 0 }]
         set data [read $channel]
         set len [llength $data]
     }
@@ -30,38 +30,44 @@ namespace eval ::sqawk::parsers::tcl {
         if {$i == $len} {
             return -code break
         }
-        if {$useDicts} {
-            if {$i == -1} {
-                set allKeys {}
-                foreach record $data {
-                    set allKeys [lsort -dictionary -unique \
-                            [concat $allKeys [dict keys $record]]]
-                }
-                # Order the keys like they are ordered in the first row for
-                # ergonomics. The keys that aren't in the first row follow those
-                # that are in alphabetical order.
-                set keys [dict keys [lindex $data 0]]
-                foreach key $allKeys {
-                    if {$key ni $keys} {
-                        lappend keys $key
-                    }
-                }
-                set row [list $keys {*}$keys]
-            } else {
-                set record [lindex $data $i]
-                set row [list $record]
-                foreach key $keys {
-                    if {[dict exists $record $key]} {
-                        lappend row [dict get $record $key]
-                    } else {
-                        lappend row {}
-                    }
+
+        if {!$useDicts} {
+            set list [lindex $data $i]
+            incr i
+            return [list $list {*}$list]
+        }
+
+        if {$i == -1} {
+            set allKeys {}
+            foreach record $data {
+                set allKeys [lsort -dictionary -unique \
+                        [concat $allKeys [dict keys $record]]]
+            }
+
+            # Order the keys like they are ordered in the first row for
+            # ergonomics.  Keys that aren't in the first row follow in
+            # alphabetical order after those that are.
+            set keys [dict keys [lindex $data 0]]
+            foreach key $allKeys {
+                if {$key ni $keys} {
+                    lappend keys $key
                 }
             }
-        } else {
-            set list [lindex $data $i]
-            set row [list $list {*}$list]
+
+            incr i
+            return [list $keys {*}$keys]
         }
+
+        set record [lindex $data $i]
+        set row [list $record]
+        foreach key $keys {
+            if {[dict exists $record $key]} {
+                lappend row [dict get $record $key]
+            } else {
+                lappend row {}
+            }
+        }
+
         incr i
         return $row
     }
